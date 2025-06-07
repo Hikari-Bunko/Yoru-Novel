856a5f311a30918f82e4c67ffed65890c43675e1
Kazue Kurosaki <137025030+KazueKuroshi@users.noreply.github.com>
2025-06-07 23:51:34 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456",
+  measurementId: "G-ABCDEF123"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const analytics = firebase.analytics();
+const auth = firebase.auth();
